61fe20fd02084ffddb5efd9e08c286ab537a4847
Victor Hugo de Souza Feracin <victorhugo.feracin@gmail.com>
2026-04-15 21:16:35 -0300

    fix(app): Fixing merge

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ const app = express();
 app.use(express.json());
 app.use("/api", bugRoutes);
 
+app.get("/docs/openapi.json", (req, res) => {
+  return res.json(buildOpenApiSpec(req));
+});
+
+app.get("/docs", (req, res) => {
+  res.type("html");
+  return res.send(renderSwaggerUiHtml());
+});
+
+app.use(bugRoutes);
+
 module.exports = app;
